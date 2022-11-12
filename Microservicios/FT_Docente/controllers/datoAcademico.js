1c9f24d1cadabbdb1994ea5e4ef49bb92df77d72
jairGil <salasun.jg@gmail.com>
2022-11-12 16:15:57 -0600

    refactor

--- Microservicios/FT_Docente/controllers/datoAcademico.js
+++ Microservicios/FT_Docente/controllers/datoAcademico.js
@@ -4,37 +4,37 @@ const dbhelper = require("../bin/db");
 const util = require("../util/util");
 
 
-let datoAcademicoController = {
-  //Agregar un nuevo usuario
-  add: async (req, res) => {
-    const connected = await dbhelper.connect();
-    console.log(connected);
+let datoAcademicoController = {}
+//Agregar un nuevo usuario
+datoAcademicoController.add = async (req, res) => {
+  const connected = await dbhelper.connect();
+  console.log(connected);
 
-    let resultSave = { action: "signin", value: false, code: 500, msg: "Error al conectar con la base de datos" }
-    if (connected.value) {
-      let datoAcademico = {};
-      const params = req.body;
+  let resultSave = { action: "signin", value: false, code: 500, msg: "Error al conectar con la base de datos" }
+  if (connected.value) {
+    let datoAcademico = {};
+    const params = req.body;
 
-      //Verificar que la cédula profesional no esté registrada
-      let resultFind = await dbhelper.findDatoAcademicoByCP(params.cedula_profesional, "signin");
-      if (resultFind.value) {
-        resultSave = await util.setResult(resultSave, false, 400, "La cédula profesional ya está registrada");
-      } else {
-        datoAcademico.grado_academico = params.grado_academico;
-        datoAcademico.doc_grado_acad = params.doc_grado_acad;
-        datoAcademico.institucion_emisora = params.institucion_emisora;
-        datoAcademico.fecha_obtencion = params.fecha_obtencion;
-        datoAcademico.cedula_profesional = params.cedula_profesional;
-        datoAcademico.doc_ced_prof = params.doc_ced_prof;
-        datoAcademico.id_docente = params.id_docente;
+    //Verificar que la cédula profesional no esté registrada
+    let resultFind = await dbhelper.findDatoAcademicoByCP(params.cedula_profesional, "signin");
+    if (resultFind.value) {
+      resultSave = await util.setResult(resultSave, false, 400, "La cédula profesional ya está registrada");
+    } else {
+      datoAcademico.grado_academico = params.grado_academico;
+      datoAcademico.doc_grado_acad = params.doc_grado_acad;
+      datoAcademico.institucion_emisora = params.institucion_emisora;
+      datoAcademico.fecha_obtencion = params.fecha_obtencion;
+      datoAcademico.cedula_profesional = params.cedula_profesional;
+      datoAcademico.doc_ced_prof = params.doc_ced_prof;
+      datoAcademico.id_docente = params.id_docente;
 
-        resultSave = await dbhelper.saveDatoAcademico(datoAcademico);
-      }
-      dbhelper.disconnect();
+      resultSave = await dbhelper.saveDatoAcademico(datoAcademico);
     }
-    console.log(resultSave);
-    return resultSave;
+    dbhelper.disconnect();
   }
-};
+  console.log(resultSave);
+  return resultSave;
+}
+
 
 module.exports = datoAcademicoController;
