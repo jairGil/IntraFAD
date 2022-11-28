3b55295b546c45acd2052896a96990b2909807df
jairGil <salasun.jg@gmail.com>
2022-11-28 02:13:16 -0600

    Datos de envio del token

--- Microservicios/DP_Docente/services/jwt.js
+++ Microservicios/DP_Docente/services/jwt.js
@@ -6,13 +6,20 @@ const secret = "AdminFAD";
 
 exports.createToken = (docente) => {
     let payload = {
-        sub: docente._id,
+        id: docente._id,
+        img: docente.img,
         nombre: docente.nombre,
         apellido_p: docente.apellido_p,
         apellido_m: docente.apellido_m,
+        direccion: docente.direccion,
+        telefono: docente.telefono,
+        correo_personal: docente.correo_personal,
+        correo_institucional: docente.correo_institucional,
         no_empleado: docente.no_empleado,
         rfc: docente.rfc,
         curp: docente.curp,
+        doc_rfc: docente.doc_rfc,
+        doc_curp: docente.doc_curp,
         ait: moment().unix(),
         exp: moment().add(1, "days").unix()
     }
